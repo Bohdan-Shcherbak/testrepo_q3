f8f8f73f372a2a80e384b384e3b6a1dd6bde9bc3
Brocoli <Bohdansherbak@gmail.com>
2022-09-07 00:34:39 +0300

    three.js

---
+++ codwars1/three.js
@@ -0,0 +1,26 @@
+// если буквы повторяются, выдаст фолс
+// сделал все большими, сделал сет, который убирает одинаковые буквы и сравнил длины Гениально
+function isIsogram(str){
+    return new Set(str.toUpperCase()).size == str.length;
+  }
+
+  function isIsogram(str){
+    str = str.toLowerCase();
+    let str1 = '';
+    let result;
+  for(let i = 0;i<=str.length;i++){
+    if(str1.includes(str[i]) == true){
+      result = false;
+      console.log('yes');
+      break;
+    } else {
+      result = true;
+      console.log('no');
+  
+    }
+    str1 += str[i];
+  
+    console.log(str1);
+  }
+  return result
+  }
--- test.js
+++ test.js
@@ -1,4 +1,37 @@
+function isIsogram(str){
+  str = str.toLowerCase();
+  let str1 = '';
+  let result;
+for(let i = 0;i<=str.length;i++){
+  if(str1.includes(str[i]) == true){
+    result = false;
+    console.log('yes');
+    break;
+  } else {
+    result = true;
+    console.log('no');
+
+  }
+  str1 += str[i];
+
+  console.log(str1);
+}
+return result
+}
 
 
+function isIsogram(str){
+  let result;
+
+function rfr(a,b){
+  // if(a == b){
+  //   return false;
+  // } else { return true}
+  return a == b? result = false : result = true;
+}
+str.replace(/a-z/gi, rfr(a,z));
+return result
+}
+console.log(isIsogram('eerte'));
 
 //   node test.js
